3df5d163b86e6666871bced6bd7d0043c8c1db8c
Martin Grellet <martin.alejandro.grellet@gmail.com>
2023-11-12 15:39:37 -0300

    temp commit

---
+++ readme.md
@@ -0,0 +1,8 @@
+First steps
+
+npm install @ionic/angular@7
+
+npm install -g @ionic/cli
+
+ionic start
+
---
+++ src/app/interfaces/expense.ts
@@ -0,0 +1,7 @@
+export interface Expense {
+  type: string;
+  amount: number;
+  description: string;
+  month: number;
+  year: number;
+}
